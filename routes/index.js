be2f56716a5b38b4357ed022215c563f2b888e35
cjj1120 <cjj8168@gmail.com>
2020-09-09 00:45:48 +0800

    Home page setup with EJS

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+module.exports = router;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home', { title: 'ExpressHome' });
 });
 
 module.exports = router;
